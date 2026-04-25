5e0027f92a966a8791d25110f39a4ed14f570aac
lemily-coder <ilyasslmhani@gmail.com>
2026-04-25 11:51:41 +0200

    one file -> many files

--- connection.h
+++ connection.h
@@ -4,9 +4,9 @@
 #include "tcp_relay_server.h"
 
 bool ensure_connection_capacity(ConnectionList *list);
-bool append_connection(ConnectionList *list, SOCKET sock, const char *label);
+bool append_connection(ConnectionList *list, int sock, const char *label);
 void remove_connection(ConnectionList *list, size_t index);
-int send_all(SOCKET sock, const char *data, int len);
+int send_all(int sock, const char *data, int len);
 void broadcast_message(ConnectionList *list, size_t sender_index, const char *line, int line_len);
 bool grow_line_buffer(Connection *c);
 bool handle_incoming_data(ConnectionList *list, size_t sender_index, const char *data, int len);
--- server.h
+++ server.h
@@ -3,9 +3,9 @@
 
 #include "tcp_relay_server.h"
 
-SOCKET init_server(unsigned short listen_port);
-bool accept_new_connection(SOCKET listen_sock, ConnectionList *list);
+int init_server(unsigned short listen_port);
+bool accept_new_connection(int listen_sock, ConnectionList *list);
 bool connect_to_peer(ConnectionList *list, const char *ip, unsigned short port);
-void run_event_loop(SOCKET listen_sock, ConnectionList *connections);
+void run_event_loop(int listen_sock, ConnectionList *connections);
 
 #endif // SERVER_H
